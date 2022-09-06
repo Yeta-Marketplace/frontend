9cd5d17173283504a2424d07a7f67ba38bd4ca74
Mathemmagician <ramil9898@gmail.com>
2022-09-06 18:09:33 -0400

    minor update to backend url

--- src/env.tsx
+++ src/env.tsx
@@ -3,12 +3,10 @@ const env = import.meta.env.VITE_ENV;
 let envApiUrl = '';
 
 if (env === 'prod') {
-  envApiUrl = `https://api.${import.meta.env.VITE_DOMAIN_PROD}`;
+  envApiUrl = `https://${import.meta.env.VITE_BACKEND_URL}.${import.meta.env.VITE_DOMAIN_PROD}`;
 } else {
-  envApiUrl = `http://api.${import.meta.env.VITE_DOMAIN_DEV}`;
+  envApiUrl = `http://${import.meta.env.VITE_BACKEND_URL}.${import.meta.env.VITE_DOMAIN_DEV}`;
 }
-
-console.log(envApiUrl);
 
 export const apiUrl = envApiUrl;
 export const appName = import.meta.env.VITE_NAME;
