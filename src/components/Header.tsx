233551c8597732760a3e845b684d9229a9268e7d
Mathemmagician <ramil9898@gmail.com>
2022-09-14 12:14:22 -0400

    added styled-components instead of css files

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,17 @@
-import './Header.css'
 import { apiUrl } from '../env'
 import { Link } from 'react-router-dom'
+import styled from "styled-components"
+
+export const AppHeader = styled.div`
+  background-color: #c1c5cf;
+  height: 10vh;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-around;
+  font-size: calc(10px + 2vmin);
+  color: rgb(29, 22, 0);
+`
 
 
 type Props = {
@@ -9,14 +20,14 @@ type Props = {
 
 function Header({ loggedIn }: Props) {
   return (
-    <header className="App-header">
+    <AppHeader>
       <h1> <Link to='/'>Yeta</Link> </h1>
       {loggedIn === true
         ? <Link to='/logout'>Sign Out</Link>
         : <Link to='/login'>Sign In</Link>
       }
       <small> <a href={apiUrl + "/docs"} style={{ color: "grey" }}>API docs</a> </small>
-    </header>
+    </AppHeader>
   )
 }
 
--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,11 +1,22 @@
-import './Home.css'
+import styled from "styled-components"
+
+export const AppHeader = styled.div`
+  background-color: #282c34;
+  min-height: 90vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  font-size: calc(10px + 2vmin);
+  color: white;
+`
 
 
-function Home({}) {
+function Home({ }) {
   return (
-    <div className="App-Home">
-        <h1> <mark>Yet A</mark> nother Marketplace</h1>
-    </div>
+    <AppHeader>
+      <h1> <mark>Yet A</mark> nother Marketplace</h1>
+    </AppHeader>
   )
 }
 
