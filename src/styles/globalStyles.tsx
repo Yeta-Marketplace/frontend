a09b69f418a7bf29213608a113cfd66b2c4aa912
Mathemmagician <ramil9898@gmail.com>
2022-09-19 19:13:33 -0400

    moving index.css to globalStyles

--- src/styles/globalStyles.tsx
+++ src/styles/globalStyles.tsx
@@ -1,13 +1,22 @@
 import { createGlobalStyle } from "styled-components";
+import "@fontsource/roboto"
 
 import { colors } from "./colors"
 
 export const GlobalStyles = createGlobalStyle`
   body {
+    margin: 0;
     font-family: "Roboto";
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
   }
 
   a {
     text-decoration: none;
   }
+
+  code {
+   font-family: source-code-pro, Menlo, Monaco, Consolas, 'Courier New',
+    monospace;
+ }
 `
