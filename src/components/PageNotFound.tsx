b7b1188cd4bd1e19e134bdc04f5cacfd51dd3a3e
Mathemmagician <ramil9898@gmail.com>
2022-09-27 17:01:38 -0400

    page not found update

--- src/components/PageNotFound.tsx
+++ src/components/PageNotFound.tsx
@@ -1,3 +1,4 @@
+import Box from '@mui/material/Box'
 import React from 'react'
 import { Link } from 'react-router-dom'
 
@@ -5,7 +6,17 @@ type Props = {}
 
 function PageNotFound({ }: Props) {
     return (
-        <div><h1>404 - Page Not Found || Return <Link to='/'>Home</Link></h1></div>
+        <Box
+            sx={{
+                marginTop: 8,
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'center',
+                textAlign: 'center'
+            }}
+        >
+            <h1>404 - Page Not Found || Return <Link to='/'>Home</Link></h1>
+        </Box>
     )
 }
 
